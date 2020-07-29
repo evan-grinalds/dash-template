41ad315213b40b81a92ee7e46ba4d64feb2d8dcd
Evan Grinalds <61983308+evan-grinalds@users.noreply.github.com>
2020-07-29 11:55:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -17,30 +17,8 @@ with the external_stylesheets argument of the Dash constructor:
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
 
-Go to https://bootswatch.com/ to preview these Bootswatch themes:
+Go to https://bootswatch.com/
 
-dbc.themes.BOOTSTRAP
-dbc.themes.CERULEAN
-dbc.themes.COSMO
-dbc.themes.CYBORG
-dbc.themes.DARKLY
-dbc.themes.FLATLY
-dbc.themes.JOURNAL
-dbc.themes.LITERA
-dbc.themes.LUMEN
-dbc.themes.LUX
-dbc.themes.MATERIA
-dbc.themes.MINTY
-dbc.themes.PULSE
-dbc.themes.SANDSTONE
-dbc.themes.SIMPLEX
-dbc.themes.SKETCHY
-dbc.themes.SLATE
-dbc.themes.SOLAR
-dbc.themes.SPACELAB
-dbc.themes.SUPERHERO
-dbc.themes.UNITED
-dbc.themes.YETI
 """
 
 external_stylesheets = [
@@ -56,44 +34,3 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=m
 app.config.suppress_callback_exceptions = True # see https://dash.plot.ly/urls
 app.title = 'Tesla Predictor' # appears in browser title bar
 server = app.server
-
-# -*- coding: utf-8 -*-
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.express as px
-import pandas as pd
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-])
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
