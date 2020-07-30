278a29fa28cea6d87a103e1626008b4923b64c72
Evan Grinalds <61983308+evan-grinalds@users.noreply.github.com>
2020-07-29 17:29:37 -0700

    Update predictions.py

--- pages/predictions.py
+++ pages/predictions.py
@@ -1,17 +1,32 @@
 # Imports from 3rd party libraries
 import dash
-import dash_daq as daq
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-from joblib import load
-import pandas as pd
 
 # Imports from this application
 from app import app
 
 # 2 column layout. 1st column width = 4/12
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
+column1 = dbc.Col(
+    [
+        dcc.Markdown(
+            """
+        
+            ## Predictions
+            Your instructions: How to use your app to get new predictions.
+            """
+        ),
+    ],
+    md=4,
+)
 
-#layout = dbc.Row([column1, column2])
+column2 = dbc.Col(
+    [
+
+    ]
+)
+
+layout = dbc.Row([column1, column2])
