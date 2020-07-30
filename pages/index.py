eea2935d1e235859e98fa61f5478dc9013209954
Evan Grinalds <61983308+evan-grinalds@users.noreply.github.com>
2020-07-30 10:26:07 -0700

    Update index.py

--- pages/index.py
+++ pages/index.py
@@ -1,4 +1,3 @@
-# Imports from 3rd party libraries
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
@@ -16,29 +15,23 @@ column1 = dbc.Col(
         dcc.Markdown(
             """
         
-            ## Your Value Proposition
+            ### Wanna go electric?
 
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
-
-            ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
-
-            ❌ RUN is the only intelligent running app that uses sophisticated deep neural net machine learning to make your run smarter because we believe in ML driven workouts.
-
+            More electric cars are on the road everyday. This makes you wonder, 
+            can I afford one? This application answers that question by 
+            predicting which model has the best value.
+            
             """
         ),
-        dcc.Link(dbc.Button('Your Call To Action', color='primary'), href='/predictions')
+        dcc.Link(dbc.Button('Predict Price', color='primary'), href='/predictions')
     ],
     md=4,
+    align='center'
 )
 
-gapminder = px.data.gapminder()
-fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
-           hover_name="country", log_x=True, size_max=60)
-
-column2 = dbc.Col(
-    [
-        dcc.Graph(figure=fig),
-    ]
+column2 = dbc.Col([html.Img(src='assets/teslamodels.jpeg', className='img-fluid')
+],
+align='center'
 )
 
 layout = dbc.Row([column1, column2])
